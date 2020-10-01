2168bb50fd355ae89026dad13cdb6a8c1dad9c76
johanperjulius1 <johan.per.julius.berglund@gmail.com>
2020-10-01 17:53:53 +0200

    Modifying ids in userCanLogin spec, creating a login form component

--- cypress/integration/userCanLogin.feature.js
+++ cypress/integration/userCanLogin.feature.js
@@ -3,21 +3,21 @@ describe("User authenticates", () => {
     cy.visit("/");
   });
   it("successfully with valid credentials", () => {
-    cy.get("#login").click();
-    cy.get("#login-form").within(() => {
-      cy.get("#email").type("user@mail.com");
-      cy.get("#password").type("password");
-      cy.get('button').contains('Submit').click()
+    cy.get('[data-cy="login"]').click();
+    cy.get('[data-cy="login-form"]').within(() => {
+      cy.get('[data-cy="email"]').type("user@mail.com");
+      cy.get('[data-cy="password"]').type("password");
+      cy.get('[data-cy="button"]').contains('Submit').click()
     });
-    cy.get("#message").should("contain", "Hi user@mail.com");
+    cy.get('[data-cy="message"]').should("contain", "Hi user@mail.com");
   });
   it("unsuccessfully with invalid credentials", () => {
-    cy.get("#login").click();
-    cy.get("#login-form").within(() => {
-      cy.get("#email").type("user@mail.com");
-      cy.get("#password").type("wrongpassword");
-      cy.get('button').contains('Submit').click()
+    cy.get('[data-cy="login"]').click();
+    cy.get('[data-cy="login-form"]').within(() => {
+      cy.get('[data-cy="email"]').type("user@mail.com");
+      cy.get('[data-cy="password"]').type("wrongpassword");
+      cy.get('[data-cy="button"]').contains('Submit').click()
     });
-    cy.get("#message").should("contain", "Invalid login credentials. Please try again.");
+    cy.get('[data-cy="message"]').should("contain", "Invalid login credentials. Please try again.");
   });
 });
